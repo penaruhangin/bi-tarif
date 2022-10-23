aca47e205258b2d72b072ae9a6b7461de5ebea47
penaruhangin <penaruhangin@gmail.com>
2022-10-23 14:25:43 +0700

    edit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import Image from 'next/image'
 
-import banner from "../public/banner.jpeg"
+import slider from "../public/slider.gif"
 import ojk from "../public/ojk.png"
 import lps from "../public/lps.png"
 import { useRouter } from 'next/router'
